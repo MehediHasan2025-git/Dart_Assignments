4d19e3d0c764e63670cb4367a6394f89e71c5501
MehediHasan2025-git <lumehedihasan71@gmail.com>
2025-10-11 15:03:06 +0600

    Create Que_04.dart

--- assignment 01/Que_04.dart
+++ assignment 01/Que_04.dart
@@ -1,1 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("Enter principal amount:");
+  double p = double.parse(stdin.readLineSync()!);
+
+  print("Enter time in years:");
+  double t = double.parse(stdin.readLineSync()!);
+
+  print("Enter rate of interest:");
+  double r = double.parse(stdin.readLineSync()!);
+
+  double SI = (p * t * r) / 100;
+  print("Simple Interest is: $SI");
+}
 
